c4aa35cb6d86625fa166da0e268b607c48ad0b05
TranThanhKhang <khangkt666@gmail.com>
2024-04-03 18:03:43 +0700

    User and Login UI

---
+++ lib/page/Loginwidget.dart
@@ -0,0 +1,108 @@
+import 'package:buoi4/main.dart';
+import 'package:buoi4/mainpage.dart';
+import 'package:flutter/material.dart';
+
+class LoginWidget extends StatefulWidget {
+  const LoginWidget({Key? key}) : super(key: key);
+
+  @override
+  _LoginWidgetState createState() => _LoginWidgetState();
+}
+
+class _LoginWidgetState extends State<LoginWidget> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _emailController,
+                     decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your email';
+                      }
+                      final emailRegex =
+                          RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+                      if (!emailRegex.hasMatch(value)) {
+                        return 'Please enter a valid email address';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    obscureText: true,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your password';
+                      }
+                      if (value.length < 8) {
+                        return 'Password must be at least 8 characters long';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                     style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.black),
+                      padding:EdgeInsets.symmetric(vertical: 12, horizontal: 50),
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => const Mainpage()));
+                      }
+                    },
+                    child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      // Navigate to the sign-up page
+                    },
+                    child:const  Text(
+                      'Don\'t have an account? Sign up here',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/page/product/productwidget.dart
+++ lib/page/product/productwidget.dart
@@ -13,88 +13,92 @@ class ProductWidget extends StatefulWidget {
 
 class _ProductWidgetState extends State<ProductWidget> {
   List<Product> lstProduct = [];
-  List<Product> lstEarPhone=[];
-  List<Product> lstAccessory=[];
+  List<Product> lstEarPhone = [];
+  List<Product> lstAccessory = [];
 
   @override
   void initState() {
     super.initState();
     lstProduct = createDataList(10);
-    lstEarPhone= createDataListEarPhone(10);
-    lstAccessory=createDataAccessory(10);
+    lstEarPhone = createDataListEarPhone(10);
+    lstAccessory = createDataAccessory(10);
   }
 
- @override
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child: Text(
-            'Categories',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+    return Container(
+      color: Color.fromRGBO(245, 241, 228, 1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Text(
+              'Categories',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: 50, //chieu cao
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: 3, //so luong trong cate
-            itemBuilder: (context, index) {
-              String categoryName = '';
-              List<Product> productList = [];
-              if (index == 0) {
-                categoryName = 'Phone Products';
-                productList = lstEarPhone;
-              } else if (index == 1) {
-                categoryName = 'EarPhone Products';
-                productList = lstProduct;
-              } else if (index == 2) {
-                categoryName = 'Accessory Products';
-                productList = lstAccessory;
-              }
-              return Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                  //them hanh dong loc
-                  },
-                 style: OutlinedButton.styleFrom(
-                    side: BorderSide(color: Colors.black), // Set the border color to black
+          SizedBox(
+            height: 50, //chieu cao
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 3, //so luong trong cate
+              itemBuilder: (context, index) {
+                String categoryName = '';
+                List<Product> productList = [];
+                if (index == 0) {
+                  categoryName = 'Phone Products';
+                  productList = lstEarPhone;
+                } else if (index == 1) {
+                  categoryName = 'EarPhone Products';
+                  productList = lstProduct;
+                } else if (index == 2) {
+                  categoryName = 'Accessory Products';
+                  productList = lstAccessory;
+                }
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      //them hanh dong loc
+                    },
+                    style: OutlinedButton.styleFrom(
+                      side: BorderSide(color: Colors.black),
+                    ),
+                    child: Text(categoryName),
                   ),
-                  child: Text(categoryName),
-                ),
-              );
-            },
+                );
+              },
+            ),
           ),
-        ),
-        Expanded(
-          //tao danh muc san pham
-          child: ListView.builder(
-            itemCount:3, // so luong danh muc san pham hien 
-            itemBuilder: (context, index) {
-              String categoryName = '';
-              List<Product> productList = [];
+          Divider(),
+          Expanded(
+            //tao danh muc san pham
+            child: ListView.builder(
+              itemCount: 3, // so luong danh muc san pham hien
+              itemBuilder: (context, index) {
+                String categoryName = '';
+                List<Product> productList = [];
 
-              if (index == 0) {
-                categoryName = 'Phone Products';
-                productList = lstEarPhone;
-              } else if (index == 1) {
-                categoryName = 'EarPhone Products';
-                productList = lstProduct;
-              } else if (index == 2) {
-                categoryName = 'Accessory Products';
-                productList = lstAccessory;
-              }
-              return _buildProductCategory(categoryName, productList);
-            },
+                if (index == 0) {
+                  categoryName = 'Phone Products';
+                  productList = lstEarPhone;
+                } else if (index == 1) {
+                  categoryName = 'EarPhone Products';
+                  productList = lstProduct;
+                } else if (index == 2) {
+                  categoryName = 'Accessory Products';
+                  productList = lstAccessory;
+                }
+                return _buildProductCategory(categoryName, productList);
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -107,7 +111,7 @@ class _ProductWidgetState extends State<ProductWidget> {
     return lstProduct;
   }
 
-    List<Product> createDataListEarPhone(int amount) {
+  List<Product> createDataListEarPhone(int amount) {
     List<Product> lstProduct = [];
     for (int i = 1; i <= amount; i++) {
       lstProduct.add(Product(
@@ -115,69 +119,17 @@ class _ProductWidgetState extends State<ProductWidget> {
     }
     return lstProduct;
   }
-      List<Product> createDataAccessory(int amount) {
+
+  List<Product> createDataAccessory(int amount) {
     List<Product> lstProduct = [];
     for (int i = 1; i <= amount; i++) {
-      lstProduct.add(Product(
-          i, "Accessory $i", i * 10000, "img_$i.jpg", "Accessory"));
+      lstProduct.add(
+          Product(i, "Accessory $i", i * 10000, "img_$i.jpg", "Accessory"));
     }
     return lstProduct;
   }
+}
 
-}
-Widget itemGridView(Product productModel) {
-  return Container(
-    padding: const EdgeInsets.all(8),
-    decoration: BoxDecoration(
-      color: Colors.grey.shade200,
-      border: Border.all(color: Colors.black), 
-    ),
-    width: 200, // Set a fixed width for each item
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Image.asset(
-          pro_img + productModel.img!,
-          height: 100,
-          width: 100,
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              productModel.name ?? '',
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Price: ${NumberFormat('###,###.###').format(productModel.price)}",
-              style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.red), // Set the text color to red
-            ),
-            Text(
-              productModel.des!,
-              style: const TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        
-        OutlinedButton(
-          onPressed: () {
-            // Add your buy now action here
-          },
-          style: OutlinedButton.styleFrom(
-            side: BorderSide(color: Colors.black), // Set the border color to black
-          ),
-          child: Text('BUY NOW'),
-        ),
-      ],
-    ),
-  );
-}
 Widget _buildProductCategory(String categoryName, List<Product> productList) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
